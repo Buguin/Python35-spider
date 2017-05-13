07747de2b74210598a5100e798601b943a3d3d72
Buguin <wybuguin@hotmail.com>
2017-05-14 02:21:38 +0800

    03 创建Request模拟浏览器
    Signed-off-by: Buguin <wybuguin@hotmail.com>

--- NewsSpider.py
+++ NewsSpider.py
@@ -2,15 +2,26 @@
 __author__ = 'Buguin'
 
 import urllib.request
+import http.cookiejar
 
 
-url = 'http://www.baidu.com/'
-req = urllib.request.Request(url, headers={
+# head: dict of header
+def myopener(head={
     'Connection': 'Keep-Alive',
     'Accept': 'text/html, application/xhtml+xml, */*',
     'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'
-})
-oper = urllib.request.urlopen(req)
-data = oper.read()
+}):
+    cj = http.cookiejar.CookieJar()
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+    header = []
+    for key, value in head.items():
+        elem = (key, value)
+        header.append(elem)
+    opener.addheaders = header
+    return opener
+
+oper = myopener()
+uop = oper.open('http://www.baidu.com/', timeout=1000)
+data = uop.read()
 print(data.decode())
