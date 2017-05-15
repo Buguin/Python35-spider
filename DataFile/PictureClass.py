956ffe1d310155adeb99a81e20ee4bd72e750937
Buguin <wybuguin@hotmail.com>
2017-05-16 01:48:46 +0800

    08 添加自定义爬虫库
    Signed-off-by: Buguin <wybuguin@hotmail.com>

---
+++ DataFile/PictureClass.py
@@ -0,0 +1,14 @@
+# -*- codeing:utf-8 -*-
+__author__ = 'Buguin'
+
+
+class Picture(object):
+    path = ''
+    name = ''
+    data = ''
+
+    def get_picture(self):
+        temp = 'path + name'
+        return temp
+
+
