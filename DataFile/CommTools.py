58c5f05c16a85562f2a51dd989cda6951de16665
Buguin <wybuguin@hotmail.com>
2017-05-20 02:26:35 +0800

    14 实现使用随机的user-agent
    Signed-off-by: Buguin <wybuguin@hotmail.com>

---
+++ DataFile/CommTools.py
@@ -0,0 +1,10 @@
+# -*- codeing:utf-8 -*-
+__author__ = 'Buguin'
+
+
+def strip_folder_name(folder_name):
+    folder_name = folder_name.strip()
+    folder_name = folder_name.strip('?')
+    return folder_name
+
+
--- DataFile/ImgSpider.py
+++ DataFile/ImgSpider.py
@@ -32,61 +32,9 @@ class ImgSpider:
             "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
         ]
 
-    def get(self, url):
+    def rq_rdheaders_opener(self, url, timeout=2):
         random_user_agent = random.choice(self.user_agent_list)
         headers = {'User-Agent': random_user_agent}
-        response = requests.get(url, headers=headers)
-        return response
+        start_html = requests.get(url, headers=headers, timeout=timeout)
+        return start_html
 
-
-    def all_url(self, url):
-        html = self.request(url)
-        all_a = BeautifulSoup(html.text, 'lxml').find('div', class_='all').find_all('a')
-        for a in all_a:
-            title = a.get_text()
-            print(u'开始保存：', title)
-            path = str(title).replace("?", '_')
-            self.mkdir(path)
-            href = a['href']
-            self.html(href)
-
-    def html(self, href):
-        html = self.request(href)
-        max_span = BeautifulSoup(html.text, 'html.parser').find('div', class_='pagenavi').find_all('span')[-2].get_text()
-        for page in range(1, int(max_span) + 1):
-            page_url = href + '/' + str(page)
-        self.img(page_url)
-
-    def img(self, page_url):
-        img_html = self.request(page_url)
-        img_url = BeautifulSoup(img_html.text, 'html.parser').find('div', class_='main-image').find('img')['src']
-        self.save(img_url)
-
-    def save(self, img_url):
-        name = img_url[-9:-4]
-        img = self.request(img_url)
-        f = open(name + '.jpg', 'ab')
-        f.write(img.content)
-        f.close()
-
-    def mkdir(self, path):
-        path = path.strip()
-        is_exists = os.path.exists(os.path.join("D:\mzitu", path))
-        if not is_exists:
-            print(u'建了一个名字叫做', path, u'的文件夹！')
-            os.makedirs(os.path.join("D:\mzitu", path))
-            os.chdir(os.path.join("D:\mzitu", path))
-            return True
-        else:
-            print(u'名字叫做', path, u'的文件夹已经存在了！')
-            return False
-
-    def request(self, url):
-        headers = {
-            'Accept': 'text/html, application/xhtml+xml, */*',
-            'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
-            'Accept-Encoding': 'gzip, deflate'
-        }
-        content = requests.get(url, headers=headers)
-        return content
