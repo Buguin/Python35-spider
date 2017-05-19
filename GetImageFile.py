3150027a169ae4ddb3a75596914d0a3e346a8ffb
Buguin <wybuguin@hotmail.com>
2017-05-20 01:12:58 +0800

    13 格式调整
    Signed-off-by: Buguin <wybuguin@hotmail.com>

--- GetImageFile.py
+++ GetImageFile.py
@@ -8,16 +8,6 @@ import os
 __author__ = 'Buguin'
 
 url_all = 'http://www.mzitu.com/all'
-headers = {
-    'Accept': 'text/html, application/xhtml+xml, */*',
-    'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
-    'Accept-Encoding': 'gzip, deflate',
-}
-# opener = urllib_opener()
-# start_html = opener.open(url_all, timeout=2)
-# html_data = start_html.read()
-# print(html_data.decode())
 start_html = request_opener(url_all)
 # start_html = requests.get(url_all, headers=headers, timeout=2)
 soup = BeautifulSoup(start_html.text, 'html.parser')
